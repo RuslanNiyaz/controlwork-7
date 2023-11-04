2f1ef9bc9531cd9dfec5d171d15840c706e377c9
Niiazov Ruslan <niyazovruslan1996@gmail.com>
2023-11-04 18:48:23 +0600

    Add items block

---
+++ src/components/AddItems/AddItems.tsx
@@ -0,0 +1,22 @@
+import {FC} from 'react';
+import {ItemsMenu} from '../../types';
+
+interface AddItemsProps {
+    item: ItemsMenu;
+    handleItemClick: (name: string, price: number) => void;
+}
+
+const AddItems: FC<AddItemsProps> = ({ item, handleItemClick }) => {
+    return (
+        <div className="addItemsMenu">
+            <button
+                className="itemBtn" key={item.id} onClick={() => handleItemClick(item.name, item.price)}>
+                <img className="btnImage" src={item.image} alt=""/>
+                {item.name}
+                <p className="costItem"> {item.price} KGS </p>
+            </button>
+        </div>
+    );
+};
+
+export default AddItems;
